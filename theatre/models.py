056b8236c48bdd319db5126e0d6022b632af9e7b
Oleh Klivanets <oleg.klivanets@gmail.com>
2026-06-16 16:19:19 +0300

    feat: add base models(actor, genre, theatre hall and play)

--- theatre/models.py
+++ theatre/models.py
@@ -1,3 +1,53 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ["last_name", "first_name"]
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class TheatreHall(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    rows = models.PositiveIntegerField(validators=[MinValueValidator(1)])
+    seats_in_row = models.PositiveIntegerField(validators=[MinValueValidator(1)])
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def capacity(self):
+        return self.rows * self.seats_in_row
+
+
+class Play(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    actors = models.ManyToManyField(Actor, related_name="plays")
+    genres = models.ManyToManyField(Genre, related_name="plays")
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self):
+        return self.title
+
